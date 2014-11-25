aa640391d135830975f79af2d3fd3adba0bea7b3
Alexandra Gurova <alexandragurova@yandex.ru>
2014-11-25 22:25:48 +0000

    Add Product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+    validates       :title, presence: true, 
+                            uniqueness: true
+    validates :description, presence: true
+    validates   :image_url, presence: true, 
+                            # allow_blank: true,
+                            format: { with: %r{\.jpg|png|gif\Z}i, 
+                                      message: "must be a URL for JPG, PNG or GIF" }
+    validates       :price, numericality: { greater_than_or_equal_to: 0.01 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:image_url].any?
+    assert product.errors[:price].any?
+  end
+  
+  test "title should be unique" do
+    product = Product.new(title: products(:ruby).title,
+                              description: "BlahBlahBlah",
+                              image_url: "image.png",
+                              price: 12.00)
+    assert product.invalid?
+    assert_equal [I18n.translate("errors.messages.taken")], product.errors[:title]
+  end
+  
+  test "image url should be valid" do
+    product = Product.new(title: "GoF Patterns",
+                    description: "Cool Book",
+                    price: 15.00)
+    ok_url = %w{ book.png book.gif book.JPG Book.Gif http://a.b.c./x/y/z/book.png }
+    bad_url = %w{ book.doc book.gi book.gif/more book.gif.more }
+    
+    ok_url.each do |image|
+      product.image_url = image
+      assert product.valid?, "#{image} should be valid"
+    end
+    
+    bad_url.each do |image|
+      product.image_url = image
+      assert product.invalid?, "#{image} should not be valid"
+    end
+  end
+  
+  test "price should be positive" do
+    product = Product.new(title: "GoF Patterns",
+                    description: "Cool Book",
+                    image_url: "image.gif")
+                    
+    product.price = -1
+    assert product.invalid? 
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price] 
+    
+    product.price = 0
+    assert product.invalid? 
+    assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
+    product.price = 1
+    assert product.valid?
+  end
 end
